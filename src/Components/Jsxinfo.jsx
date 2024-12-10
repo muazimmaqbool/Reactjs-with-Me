8ce0ff9952f65f99fd127cd125f25803dc5c6377
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-10 21:30:06 +0530

    Jsx info in react

--- src/Components/Jsxinfo.jsx
+++ src/Components/Jsxinfo.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
+//Note before going throgh the code react the documentation below
 const Jsxinfo = () => {
   const x = 20;
   let text = "Hello React";
   if (x < 25) {
     text = "Hello JSX";
   }
-  //we can't use if inside jsx
-  //read document below
   return (
     <>
       <h2>Inside Jsxinfo file</h2>
@@ -15,7 +14,7 @@ const Jsxinfo = () => {
       <h3>sum of 10 and 5: {10 + 5}</h3>
       <h3>value of x is: {x}</h3>
       <h3>text: {text}</h3>
-      <h3>ternary expressions inside jsx</h3>
+      <h3>ternary expressions inside jsx:</h3>
       <h3>using ternary o/p : {x < 15 ? "less then 15" : "greater then 15"}</h3>
     </>
   );
@@ -24,7 +23,7 @@ const Jsxinfo = () => {
 export default Jsxinfo;
 
 /*
-JSX info:
+  ->JSX info:
     JSX: means javascript XML, its an extention of javascript syntax, which is used in react to write html and javascript together. 
     >It makes easy to write react code
     >JSX coverts html tags in react elements
@@ -32,20 +31,19 @@ JSX info:
     >JSX makes it easier to write and add html in react
     >JSX is translated into regular J.S at runtime
  
- ->Expressions in JSX
+  ->Expressions in JSX
     With JSX you can write expressions inside curly braces { }.
     The expression can be a React variable, or property, or any other valid JavaScript expression.
-     JSX will execute the expression and return the result:
-     <h1>React is {5 + 5} times better with JSX</h1>
+    JSX will execute the expression and return the result:
+    example : <h1>React is {5 + 5} times better with JSX</h1>
 
- -> Important:
+  -> Important:
     >JSX will throw an error if the HTML is not correct, or if the HTML misses a parent element.
     >Elements Must be Closed
-    >JSX follows XML rules, and therefore HTML elements must be properly closed.
-       -> Close empty elements with />
+    >JSX follows XML rules, and therefore HTML elements must be properly closed. -> Close empty elements with />
     JSX will throw an error if the HTML is not properly closed.
 
-->Attribute class = className:
+  ->Attribute class = className:
     The class attribute is a much used attribute in HTML, but since JSX is rendered as JavaScript, 
     and the class keyword is a reserved word in JavaScript, you are not allowed to use it in JSX.
     Use attribute className instead.
@@ -53,7 +51,7 @@ JSX info:
     attributes into class attributes.
         Example: <h1 className="myclass">Hello World</h1>;
 
-->Conditions - if statements
+  ->Conditions - if statements
     React supports if statements, but not inside JSX.
     To be able to use conditional statements in JSX, you should put the if statements outside
     of the JSX, or you could use a ternary expression instead:
@@ -65,7 +63,7 @@ JSX info:
     then inside return:
                 <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
 
-->Note that in order to embed a JavaScript expression inside JSX, the JavaScript must be wrapped
- with curly braces, {}.
- >JSX is only written inside return
+  ->Note that in order to embed a JavaScript expression inside JSX, the JavaScript must be wrapped
+    with curly braces, {}.
+    ->JSX is only written inside return
     */
