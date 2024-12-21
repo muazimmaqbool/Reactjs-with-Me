67497c7d5bdd8ff4ceb33dbacdc60e38b370df89
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-21 16:56:21 +0530

    useEffect intro

--- src/Hooks/UseEffect.jsx
+++ src/Hooks/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
+//Read Documentation Below:
 const UseEffect = () => {
   return (
     <>
@@ -29,9 +30,9 @@ export default UseEffect
 /*
 -> useEffect hook: (also study from notes)
     The useEffect Hook allows you to perform side effects in your components.
-    Some examples of side effects are: fetching data, directly updating the DOM, and timers.
-    ->useEffect is basically a function which is run when component loads or when component is updated
-    useEffect accepts two arguments. function and the dependency. The second argument is optional.
+    Some examples of side effects are: fetching data, directly updating the DOM, and timers etc...
+    ->useEffect is basically a function which runs when component loads or when component is updated
+    useEffect accepts two arguments. function and the dependency.
     useEffect(<function>, <dependency>)
 
     ->Let's use a timer as an example.
@@ -39,7 +40,8 @@ export default UseEffect
 
     ->But wait!! It keeps counting even though it should only count once!
       useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect.
-      This is not what we want. There are several ways to control when side effects run.
+      This is not what we want.
+      ->There are several ways to control when side effects run.
       We should always include the second parameter which accepts an array. We can optionally pass dependencies to useEffect in this array.
       
       Example
