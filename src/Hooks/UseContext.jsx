22ddc53105b72cd10abe6192dd97441508947a5c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-22 11:21:39 +0530

    useContet info

--- src/Hooks/UseContext.jsx
+++ src/Hooks/UseContext.jsx
@@ -1,5 +1,7 @@
 import React, { useState,createContext,useContext } from 'react'
 
+//Read Documentation Below:
+
 //context can be any name
 const context=createContext();
 const UseContext = () => {
@@ -63,8 +65,8 @@ export default UseContext
 
     ->The Problem
         State should be held by the highest parent component in the stack that requires access to the state.
-        To illustrate, we have many nested components. The component at the top and bottom of the stack need 
-        access to the state.
+        To illustrate, we have many nested components. 
+        The component at the top and bottom of the stack needs access to the state.
         To do this without Context, we will need to pass the state as "props" through each nested
         component. This is called "prop drilling".
 
