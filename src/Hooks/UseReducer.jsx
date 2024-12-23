5f4412c53feb2a83ded634163439e761a4350f86
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-23 20:43:26 +0530

    useReducer example

--- src/Hooks/UseReducer.jsx
+++ src/Hooks/UseReducer.jsx
@@ -1,73 +1,70 @@
-import React,{useReducer} from 'react'
+import React, { useReducer } from "react";
 
 //Read Documentation Below:
 const UseReducer = () => {
-  return (
-   <>
-    <Example1 />
-   </>
-  )
-}
+  return <Example1 />;
+};
 
 //this ACTION object is used in reducer function its used inplace of hardcoded strings
 //this is optional but its easy and goodway (at 6:50)
-const ACTIONS={
-  INCREMENT:'increment',
-  DECREMENT:'decrement',
-}
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
 //now instead of using hardcoded strings in reducer we can just say
 //ACTIONS.INCREMENT
 //its good to use these values increment and decrement in object instead of using hardcoded strings
 //if you want to change it now we only need to change it in object ACTIONS
 
-//this reducer(anyname) takes two parameters state: where our application is currently at and 
+//this reducer(anyname) takes two parameters state: where our application is currently at and
 //action: this action is passed into dispatch function
-function reducer(state,action){
+function reducer(state, action) {
   //return {count:state.count+1} //now everytime we call dispatch its going to increment count by 1
   //for handling increment and decrement also we use switch statement here
-  switch(action.type){ //type is set in dispatch fuction
+  switch (
+    action.type //type is set in dispatch fuction
+  ) {
     //case 'increment': //instead of using hardcoded strings here we can use global object name ACTIONS
     case ACTIONS.INCREMENT: //used when ACTIONS objects is used, its optional
-      return {count:state.count+1}
+      return { count: state.count + 1 };
     //case 'decrement':
     case ACTIONS.DECREMENT:
-      return {count:state.count-1}
+      return { count: state.count - 1 };
     default:
-      return state //returning state here means do nothing with the state
+      return state; //returning state here means do nothing with the state
   }
 }
 //Here is an example of useReducer
-function Example1(){
-  const [state,dispatch] = useReducer(reducer,{count:0}) // we can also use reducer,0 then use count in place state
+function Example1() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 }); 
+  // we can also use reducer,0 then use count in place state
   // dispatch function is going to be called when we update our state
 
- // const[count,setCount]=useState(0) //now useRedcer is used
+  // const[count,setCount]=useState(0) //now useRedcer is used
 
-  function increment(){
+  function increment() {
     //setCount(prevCount=>prevCount+1) //used with useState
 
     //dispatch() //this will just increment , to make it decrement also we pass type into dispatch
-    //dispatch({type:'increment'}) //this increments 
-    dispatch({type:ACTIONS.INCREMENT})
+    //dispatch({type:'increment'}) //this increments
+    dispatch({ type: ACTIONS.INCREMENT });
   }
-  function decrement(){
+  function decrement() {
     //setCount(prevCount=>prevCount-1)
     //dispatch({type:'decrement'}) // this decrements
-    dispatch({type:ACTIONS.DECREMENT})
-
+    dispatch({ type: ACTIONS.DECREMENT });
   }
 
-  return(
+  return (
     <>
-    <button onClick={decrement}>-</button>
-    <span>{state.count}</span>
-    <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
     </>
-  )
+  );
 }
 
-export default UseReducer
-
+export default UseReducer;
 
 /*
  ->useReducer() Hook
