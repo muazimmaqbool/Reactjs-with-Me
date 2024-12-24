33190bd54806826f53ef4836555c6d98cf555a33
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-24 21:59:43 +0530

    format fix

--- src/Hooks/UseReducer2.jsx
+++ src/Hooks/UseReducer2.jsx
@@ -1,38 +1,40 @@
-import React, { useState,useReducer } from 'react'
+import React, { useState, useReducer } from "react";
 
 //First check previous code i.e UseReducer.jsx
 const UseReducer2 = () => {
-  return (
-    <Example1/>
-  )
-}
-function reducer(state,action){
-    if(action.type==='increment-task'){
-        return{
-            task:state.task+1
-        }
-    }else{
-        return{
-            task:state.task-1
-        }
-    }
+  return <Example1 />;
+};
+function reducer(state, action) {
+  if (action.type === "increment-task") {
+    return {
+      task: state.task + 1,
+    };
+  } else {
+    return {
+      task: state.task - 1,
+    };
+  }
 }
 //useReducer Example of increment Task
-const Example1=()=>{
-    const[state,dispatch]=useReducer(reducer,{task:1})
+const Example1 = () => {
+  const [state, dispatch] = useReducer(reducer, { task: 1 });
 
-    function addTask(){
-        dispatch({type:'increment-task'})
-    }
-    return(
-        <>
-        <button onClick={addTask}>ADD TASK</button>
-        <p>Pending Tasks:{state.task}</p>
-        <button onClick={()=>{
-            dispatch({type:'decrement-task'})
-        }}>Remove TASK</button>
-        </>
-    )
-}
+  function addTask() {
+    dispatch({ type: "increment-task" });
+  }
+  return (
+    <>
+      <button onClick={addTask}>ADD TASK</button>
+      <p>Pending Tasks:{state.task}</p>
+      <button
+        onClick={() => {
+          dispatch({ type: "decrement-task" });
+        }}
+      >
+        Remove TASK
+      </button>
+    </>
+  );
+};
 
-export default UseReducer2
+export default UseReducer2;
