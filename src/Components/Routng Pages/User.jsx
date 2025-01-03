687f95a0e165e7c013c7aa2ac7bf758370267c0f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-03 21:54:57 +0530

    fix

--- src/Components/Routng Pages/User.jsx
+++ src/Components/Routng Pages/User.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams , useNavigate} from 'react-router-dom'
 
+//called from Routing.jsx
 const User = () => {
   const params=useParams(); //used to get id
 
