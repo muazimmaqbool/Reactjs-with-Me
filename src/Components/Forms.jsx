f7ef7ccbdfdbdd139542772192f8ad69703c701b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-26 10:30:18 +0530

    forms info

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -52,7 +52,8 @@ export default Forms;
 /*
     React Forms:
       Just like in HTML, React uses forms to allow users to interact with the web page.
-    ->Adding Forms in React : You add a form with React like any other element: example above
+
+    -> Adding Forms in React : You add a form with React like any other element: example above
     -> In the above Myform example:
         This will work as normal, the form will submit and the page will refresh.
         But this is generally not what we want to happen in React.
@@ -63,13 +64,12 @@ export default Forms;
         Handling forms is about how you handle the data when it changes value or gets submitted.
         In HTML, form data is usually handled by the DOM.
         In React, form data is usually handled by the components.
-        When the data is handled by the components, all the data is stored in the component state.
+        When the data is handled by the components, all the data is stored in the components state.
         You can control changes by adding event handlers in the onChange attribute.
-        We can use the useState Hook to keep track of each inputs value and provide a 
+        We can use the useState Hook to keep track of each input value and provide a 
         "single source of truth" for the entire application.
 
     Submitting Forms:
-        You can control the submit action by adding an event handler in the onSubmit
-         attribute for the <form>:
+        You can control the submit action by adding an event handler in the onSubmit attribute for the <form>:
   Note: now take a look at Forms2 component
 */
