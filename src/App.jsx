8ee991c10f4065ee4ec7e5eeef07f7d119263a46
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-28 21:05:33 +0530

    memo

--- src/App.jsx
+++ src/App.jsx
@@ -82,10 +82,10 @@ function App() {
       {/* <Home/> */}
 
       {/*this component is inside SASS Styling*/}
-      <HomeSASS/> 
+      {/* <HomeSASS/>  */}
 
       {/*used to explain memo */}
-      {/* <Memo/> */}
+      <Memo/>
 
       {/* used to explain API calls using fetch and axios */}
       {/* <FetchAPI/> */}
