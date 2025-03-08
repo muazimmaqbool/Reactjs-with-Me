ee27f0075dfaf44d09f0800e5ff8c27070558aff
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-08 22:26:02 +0530

    example one

--- src/Tanstack React Query/B_ReactQuery_Ex1.jsx
+++ src/Tanstack React Query/B_ReactQuery_Ex1.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query'
 
 //Checkout previous component first
 //Example One Tanstack React Query
+
+//api call
+const fetchUsers=async()=>{
+    //console.log("Fetching users...")
+    const response=await fetch("https://jsonplaceholder.typicode.com/users")
+    if(!response.ok){
+        throw new Error("failed to fetch users")
+    }else{
+        return response.json()
+    }
+}
 const B_ReactQuery_Ex1 = () => {
+    const {data,isLoading,error}=useQuery({
+        queryKey:["users"],
+        queryFn:fetchUsers,
+    })
+    if(isLoading) return <p>Loading...</p>;
+    if(error) return <p>Error:{error.message}</p>
+    console.log("data fetched:",data)
   return (
     <div>
         <h2>Example One Of React Query</h2>
+        <h4>Users Fetched:</h4>
+        <ul>
+            {data.map((user)=>(
+                <li key={user.id}>#{user.id} : {user.name}</li>
+            ))}
+        </ul>
     </div>
   )
 }
