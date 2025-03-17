48b7c3ee20432721ae89528546548d82a646870d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-17 10:33:15 +0530

    mutate

--- src/TanstackReactQuery/Mini_Project/Component/PostList.jsx
+++ src/TanstackReactQuery/Mini_Project/Component/PostList.jsx
@@ -1,27 +1,61 @@
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import { fetchPosts } from '../API/api'
-import styles from "../projectStyles.module.css"
+import { useMutation, useQuery } from "@tanstack/react-query";
+import React from "react";
+import { addPost, fetchPosts } from "../API/api";
+import styles from "../projectStyles.module.css";
 
 //called from MainPage.jsx
 const PostList = () => {
-  const {data:postData,isLoading,isError,error}=useQuery({
-    queryKey:["posts"],
-    queryFn:fetchPosts
-  })
+  const {
+    data: postData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  //getting tags
+
+  //adding a new post by making a POST request
+  //mutate function which it returns is important, we will be calling to make the POST request
+  const {
+    mutate,
+    isError: isPostError,
+    isPending,
+    error: postError,
+    reset,
+  } = useMutation({
+    mutationFn: addPost,
+  });
   return (
     <div className={styles.container}>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter Your Post..."
+          className={styles.postBox}
+          name="title"
+
+        />
+        
+      </form>
       {isLoading && <p>Loading...</p>}
       {isError && <p>{error?.message}</p>}
-      {postData && postData.length>0 && postData.map((post)=>{
-        return <div className={styles.post} key={post.id}>
-          <div>{post.title}</div>
-          {post.tags.map((tag)=><span key={tag}>{tag}</span>)}
-        </div>
-      })}
-
+      {postData &&
+        postData.length > 0 &&
+        postData.map((post) => {
+          return (
+            <div className={styles.post} key={post.id}>
+              <div>{post.title}</div>
+              {post.tags.map((tag) => (
+                <span key={tag}>{tag}</span>
+              ))}
+            </div>
+          );
+        })}
     </div>
-  )
-}
+  );
+};
 
-export default PostList
+export default PostList;
