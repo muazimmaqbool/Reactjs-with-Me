ccea3486d7cbb27017ad7fae0a448c3e3016aed2
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-17 10:49:31 +0530

    mutate calling func

--- src/TanstackReactQuery/Mini_Project/Component/PostList.jsx
+++ src/TanstackReactQuery/Mini_Project/Component/PostList.jsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
-import { addPost, fetchPosts } from "../API/api";
+import { addPost, fetchPosts, fetchTags } from "../API/api";
 import styles from "../projectStyles.module.css";
 
 //called from MainPage.jsx
@@ -16,6 +16,10 @@ const PostList = () => {
   });
 
   //getting tags
+  const{data:tagsData,}=useQuery({
+    queryKey:["tags"],
+    queryFn:fetchTags,
+  })
 
   //adding a new post by making a POST request
   //mutate function which it returns is important, we will be calling to make the POST request
@@ -28,17 +32,38 @@ const PostList = () => {
   } = useMutation({
     mutationFn: addPost,
   });
+  const hanldeSubmit=(e)=>{
+    e.preventDefault()
+    //console.log("e.target:",e.target)
+    const formData=new FormData(e.target)
+    const title=formData.get("title")
+    const tags=Array.from(formData.keys()).filter((key)=>formData.get(key)==="on")
+    if(!title || !tags) return
+    //console.log("title:",title)
+    //console.log("tags:",tags)
+    mutate({id:postData.length+1,title,tags})
+    e.target.reset() //to reset the form
+  }
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={hanldeSubmit}>
         <input
           type="text"
           placeholder="Enter Your Post..."
-          className={styles.postBox}
+          className={styles.postbox}
           name="title"
-
         />
-        
+        <div className={styles.tags}>
+        {tagsData && tagsData.length>0 && tagsData.map((tag)=>{
+          return(
+            <div key={tag}>
+              <input name={tag} id={tag} type="checkbox"/>
+              <label htmlFor={tag}>{tag}</label>
+            </div>
+          )
+        })}
+        </div>
+        <button type="submit">POST</button>
       </form>
       {isLoading && <p>Loading...</p>}
       {isError && <p>{error?.message}</p>}
