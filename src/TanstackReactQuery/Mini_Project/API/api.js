47193e23b7655cb7fe77f8d3411f2ea7dc62a914
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-18 10:24:49 +0530

    mutation props

--- src/TanstackReactQuery/Mini_Project/API/api.js
+++ src/TanstackReactQuery/Mini_Project/API/api.js
@@ -18,7 +18,7 @@ const fetchTags = async () => {
 };
 
 const addPost = async (post) => {
-  const response = await fetch("http://localhost:3000/posts", {
+  const response = await fetch("http://localhost:3000/pots", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- src/TanstackReactQuery/Mini_Project/Component/PostList.jsx
+++ src/TanstackReactQuery/Mini_Project/Component/PostList.jsx
@@ -16,14 +16,14 @@ const PostList = () => {
   });
 
   //getting tags
-  const{data:tagsData,}=useQuery({
-    queryKey:["tags"],
-    queryFn:fetchTags,
-  })
+  const { data: tagsData } = useQuery({
+    queryKey: ["tags"],
+    queryFn: fetchTags,
+  });
 
   //adding a new post by making a POST request
   //mutate function which it returns is important, we will be calling to make the POST request
-  const queryClient=useQueryClient()
+  const queryClient = useQueryClient();
   const {
     mutate,
     isError: isPostError,
@@ -32,18 +32,23 @@ const PostList = () => {
     reset,
   } = useMutation({
     mutationFn: addPost,
-    onMutate:()=>{
-      return {id:1}
+    onMutate: () => {
+      return { id: 1 };
     },
-    onSuccess:(data,variables,context)=>{
+    onSuccess: (data, variables, context) => {
       queryClient.invalidateQueries({
-        queryKey:["posts"],
-        exact:true, //so that it can only invalidate the above query which contains "posts"
-        predicate:(query)=>query.queryKey[0]==="posts" && query.queryKey[1].page>=2,//so here you can say which query to be invalided, so here "posts" and all the pages more than 2 will be invalidated
-      })
-    }
+        queryKey: ["posts"],
+        exact: true, //so that it can only invalidate the above query which contains "posts"
+        // predicate:(query)=>query.queryKey[0]==="posts" && query.queryKey[1].page>=2,
+        //so here you can say which query to be invalided, so here "posts" and all the pages more than 2 will be invalidated
+      });
+    },
+    onError: (error, variables, context) => {
+      console.log("onError:", error);
+    },
+    onSettled: (data, error, variables, context) => {},
   });
- /*
+  /*
  here: onMutate runs before this actual mutation happens i.e before the function call
        onSuccess runs after the mutation happens i.e after function call runs successfully
       
@@ -56,22 +61,26 @@ const PostList = () => {
         queryKey:["posts"]
       }) : will invalidate the query key we provided basically it will refetch the data for this query key
        Note: exact and predicate are optional although they can be useful in some cases
+       predicate is not that much frequently used
+
+       ->onError: this takes an function and runs if the error happens
+       ->onSettled: it runs now matter what
  */
 
-
-
-  const hanldeSubmit=(e)=>{
-    e.preventDefault()
+  const hanldeSubmit = (e) => {
+    e.preventDefault();
     //console.log("e.target:",e.target)
-    const formData=new FormData(e.target)
-    const title=formData.get("title")
-    const tags=Array.from(formData.keys()).filter((key)=>formData.get(key)==="on")
-    if(!title || !tags) return
+    const formData = new FormData(e.target);
+    const title = formData.get("title");
+    const tags = Array.from(formData.keys()).filter(
+      (key) => formData.get(key) === "on"
+    );
+    if (!title || !tags) return;
     //console.log("title:",title)
     //console.log("tags:",tags)
-    mutate({id:postData.length+1,title,tags})
-    e.target.reset() //to reset the form
-  }
+    mutate({ id: postData.length + 1, title, tags });
+    e.target.reset(); //to reset the form
+  };
   return (
     <div className={styles.container}>
       <form onSubmit={hanldeSubmit}>
@@ -82,19 +91,24 @@ const PostList = () => {
           name="title"
         />
         <div className={styles.tags}>
-        {tagsData && tagsData.length>0 && tagsData.map((tag)=>{
-          return(
-            <div key={tag}>
-              <input name={tag} id={tag} type="checkbox"/>
-              <label htmlFor={tag}>{tag}</label>
-            </div>
-          )
-        })}
+          {tagsData &&
+            tagsData.length > 0 &&
+            tagsData.map((tag) => {
+              return (
+                <div key={tag}>
+                  <input name={tag} id={tag} type="checkbox" />
+                  <label htmlFor={tag}>{tag}</label>
+                </div>
+              );
+            })}
         </div>
         <button type="submit">POST</button>
       </form>
       {isLoading && <p>Loading...</p>}
       {isError && <p>{error?.message}</p>}
+
+      {isPending && <p>Posting...</p>}
+      {isPostError && <p onClick={()=>reset()}>Unable to Post</p>}
       {postData &&
         postData.length > 0 &&
         postData.map((post) => {
