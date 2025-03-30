31b3a6c8cab946709e4f9a1b1f2866af08f6f688
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-30 16:10:44 +0530

    fix

--- src/TanstackReactQuery/J_useQueriesEx2.jsx
+++ src/TanstackReactQuery/J_useQueriesEx2.jsx
@@ -5,25 +5,33 @@ import { fetchTodos, fetchUsers } from "./a_apiCalls";
 const J_useQueriesEx2 = () => {
   const result = useQueries({
     queries: [
-      { queryKey: ["users"], queryFn: () => fetchUsers() },
-      { queryKey: ["todos"], queryFn: () => fetchTodos() },
+      { queryKey: ["users"], queryFn: fetchUsers, staleTime: Infinity },
+      { queryKey: ["todos"], queryFn: fetchTodos, staleTime: Infinity },
     ],
   });
   const { usersQuery, todosQuery } = result;
-  if (usersQuery.isLoading || todosQuery.isLoading) return <p>Loading...</p>;
-  if (usersQuery.isError) return <p>Error: {usersQuery.error.message}</p>;
+  if (usersQuery?.isLoading || todosQuery?.isLoading) return <p>Loading...</p>;
+  if (usersQuery?.isError) return <p>Error: {usersQuery.error.message}</p>;
   return (
     <div>
       <h4>useQueries Hook Example Two:</h4>
-      <h2>Users</h2>
-      {usersQuery.data.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
+      {usersQuery && (
+        <>
+          <h2>Users</h2>
+          {usersQuery?.data?.map((user) => (
+            <p key={user.id}>{user.name}</p>
+          ))}
+        </>
+      )}
 
-      <h2>Posts</h2>
-      {todosQuery.data.map((post) => (
-        <p key={post.id}>{post.title}</p>
-      ))}
+      {todosQuery && (
+        <>
+          <h2>Posts</h2>
+          {todosQuery?.data?.map((todo) => (
+            <p key={todo.id}>{todo.title}</p>
+          ))}
+        </>
+      )}
     </div>
   );
 };
