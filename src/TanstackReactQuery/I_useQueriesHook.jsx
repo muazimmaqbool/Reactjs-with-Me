469acb1c673b1c47d3613db3868df847f6933670
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-03-30 15:58:18 +0530

    combine queries

--- src/TanstackReactQuery/I_useQueriesHook.jsx
+++ src/TanstackReactQuery/I_useQueriesHook.jsx
@@ -1,13 +1,37 @@
+import { useQueries } from '@tanstack/react-query'
 import React from 'react'
+import { fetchUsersID } from './a_apiCalls'
 /*
 ->useQueries is a powerful hook from TanStack Query that allows you to run multiple queries in parallel
 ->The useQueries hook can also be used to fetch a variable number of queries
   example: fetching multiple user via id
 */
 const I_useQueriesHook = () => {
-  const userIds=['1,2,3,4,5']
+  const userIds=[1,2,3,4,5]
+  const result=useQueries({
+    queries:userIds.map((id)=>({
+      queryKey:['user',id],
+      queryFn:()=>fetchUsersID(id),
+      
+    })),
+    //if you want to combine data (or other Query information) from the results into a single value, you can use the combine option
+    combine: (results) => {
+      return {
+        data: results.map((result) => result.data),
+        pending: results.some((result) => result.isPending),
+      }
+    },
+  })
+  // console.log("data:",data)
   return (
-    <div>I_useQueriesHook</div>
+    <div>useQueries Hook Example:
+      <h4>Users Fetched:</h4>
+        <ul>
+            {result && result?.data?.map((user)=>(
+                <li key={user?.id}>#{user?.id} : {user?.name}</li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
