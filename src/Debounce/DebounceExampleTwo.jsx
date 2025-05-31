2d8224313f5ed1b948a5961e9952f1d6efda9cbb
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-05-31 12:29:48 +0530

    fix

--- src/Debounce/DebounceExampleTwo.jsx
+++ src/Debounce/DebounceExampleTwo.jsx
@@ -6,15 +6,19 @@ const DebounceExampleTwo = () => {
   const [valueToSearch, setvalueToSearch] = useState("");
  // console.log("search :", search);
   useEffect(() => {
-    const time=setTimeout(() => {
+    const timeID=setTimeout(() => {
       //console.log("To Search :", search);
         setvalueToSearch(search);
     }, 1000);
     return () => {
       //console.log("useEffect clean here");
-      clearTimeout(time);
+      clearTimeout(timeID);
     };
   }, [search]);
+  /*
+  Here when user type anything in the input field, it will wait for 1 second before updating the valueToSearch state.
+  If the user types again within that 1 second, the previous timeout is cleared via return in useEffect and a new one is set.
+  */
   return (
     <div>
       <h2>Debounce Example Two</h2>
