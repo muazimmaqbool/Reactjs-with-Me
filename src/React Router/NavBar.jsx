c504dfacfd9ee268dd9421d5a7934137384d8d72
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-24 13:02:36 +0530

    Navbar data

--- src/React Router/NavBar.jsx
+++ src/React Router/NavBar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <img src='' alt='navbar image'/>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button>Get Started</button>
+    </div>
   )
 }
 
