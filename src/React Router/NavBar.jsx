7b7efe8f90f295a650de245636c8ad9f0b7c897c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-25 21:03:18 +0530

    navbar link added

--- src/React Router/NavBar.jsx
+++ src/React Router/NavBar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import logo from "../logo.png"
 import styles from "./reactRouterStyle.module.css"
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className={styles.navbar}>
       <img style={{width:70, height:70}} src={logo} alt='navbar image'/>
       <ul className={styles.list}>
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/products"><li>Products</li></Link>
+        <Link to="/about"><li>About</li></Link>
+        <Link to="/contact"><li>Contact</li></Link>
       </ul>
       <button>Get Started</button>
     </div>
