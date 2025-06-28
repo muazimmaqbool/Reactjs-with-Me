1c00f6c84cfc8c0345158c9beae8149a27c780ca
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-28 14:35:59 +0530

    NavLink

--- src/React Router/NavBar.jsx
+++ src/React Router/NavBar.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import logo from "../logo.png"
 import styles from "./reactRouterStyle.module.css"
-import { Link } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className={styles.navbar}>
       <img style={{width:70, height:70}} src={logo} alt='navbar image'/>
       <ul className={styles.list}>
-        <Link to="/"><li>Home</li></Link>
+
+        {/* <Link to="/"><li>Home</li></Link>
         <Link to="/products"><li>Products</li></Link>
         <Link to="/about"><li>About</li></Link>
-        <Link to="/contact"><li>Contact</li></Link>
+        <Link to="/contact"><li>Contact</li></Link> */}
+
+        {/* ➡️ Using NavLink instead of Link */}
+
+        <NavLink to="/"><li>Home</li></NavLink>
+        <NavLink to="/products"><li>Products</li></NavLink>
+        <NavLink to="/about"><li>About</li></NavLink>
+        <NavLink to="/contact"><li>Contact</li></NavLink>
+
       </ul>
       <button>Get Started</button>
     </div>
