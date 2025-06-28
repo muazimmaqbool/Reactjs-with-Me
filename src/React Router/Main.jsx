00a212b0f943c752139db49260c2fae29d6b23e8
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-28 16:13:48 +0530

    useNavigate hook

--- src/React Router/Main.jsx
+++ src/React Router/Main.jsx
@@ -78,11 +78,15 @@ const Main = () => {
           5)Remove BrowserRouter which is wrapping this Main.jsx file in App.jsx
           6)Use this 'router' inside return
 
-      🟢 Note: This new way will only work if your react router package is higher than 6.4
+      🟢 Note: This new way will only work if your react router package is higher than
       */}
       <RouterProvider router={router} />
     </div>
   );
 };
+/*
+➡️ See useNavigateHook in NavBar.jsx file
+
+*/
 
 export default Main;
--- src/React Router/NavBar.jsx
+++ src/React Router/NavBar.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import logo from "../logo.png"
 import styles from "./reactRouterStyle.module.css"
-import { Link,NavLink } from 'react-router-dom'
+import { Link,NavLink, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+  const navigate=useNavigate()
   return (
     <div className={styles.navbar}>
       <img style={{width:70, height:70}} src={logo} alt='navbar image'/>
       <ul className={styles.list}>
 
-        {/* <Link to="/"><li>Home</li></Link>
+        <Link to="/"><li>Home</li></Link>
         <Link to="/products"><li>Products</li></Link>
         <Link to="/about"><li>About</li></Link>
-        <Link to="/contact"><li>Contact</li></Link> */}
+        <Link to="/contact"><li>Contact</li></Link>
 
         {/* ➡️ Using NavLink instead of Link */}
 
-        <NavLink to="/"><li>Home</li></NavLink>
+        {/* <NavLink to="/"><li>Home</li></NavLink>
         <NavLink to="/products"><li>Products</li></NavLink>
         <NavLink to="/about"><li>About</li></NavLink>
-        <NavLink to="/contact"><li>Contact</li></NavLink>
+        <NavLink to="/contact"><li>Contact</li></NavLink> */}
 
       </ul>
-      <button>Get Started</button>
+      {/* when this button is clicked it will redirect us to a about page using useNavigate Hook */}
+      <button onClick={()=>navigate("/about")}>Get Started</button>
     </div>
   )
 }
@@ -35,6 +37,11 @@ const NavBar = () => {
         - Now: if you can if any route is active, you can see that, that 'a' tag have a className of 'active'
         - Noe let's add something something to this actuve className
         - So, in reactRouterStyle.module.css file, you can see styling of active className, i.e active class inside a tag i.e a.active
-*/
+
+➡️ useNavigate Hook:
+    -> useNavigate is a hook that allows you to programmatically navigate to different routes in your
+    -> like redirecting users to a different page after a form submission or a button click.
+    -> It's also used to pass state or parameters between routes.
+ */
 
 export default NavBar
