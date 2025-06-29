4b68b44aca1ec85e787b38b563f5d3b217030cbb
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-29 10:41:30 +0530

    Passing id via links

--- src/React Router/Pages/Contact.jsx
+++ src/React Router/Pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "../reactRouterStyle.module.css"
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Contact = () => {
   const navigate = useNavigate();
@@ -8,10 +8,18 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact Page</h1>
+      <ul style={{display:"flex", gap:"20px"}}>
+        <li><Link to={"1"}>Support 1</Link></li>
+         <li><Link to={"2"}>Support 2</Link></li>
+          <li><Link to={"3"}>Support 3</Link></li>
+           <li><Link to={"4"}>Support 4</Link></li>
+            <li><Link to={"5"}>Support 5</Link></li>
+      </ul>
       <div className={styles.contactBtns}>
         <button onClick={()=>navigate("info")}>Contact Info</button>
         <button onClick={()=>navigate("form")}>Contact Form</button>
       </div>
+      
     </div>
   )
 }
