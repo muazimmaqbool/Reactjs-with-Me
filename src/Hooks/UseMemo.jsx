30b12460f16aa004f45ffb231878ead180d0fd61
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-07-22 14:03:19 +0530

    useMemo example

--- src/Hooks/UseMemo.jsx
+++ src/Hooks/UseMemo.jsx
@@ -19,7 +19,7 @@ const ExampleOne=()=>{
     //we can use the useMemo Hook to memoize the expensiveCalculation function. This will cause the function to only run when needed.
     //We can wrap the expensive function call with useMemo:
     const calculation=useMemo(()=>expensiveCalculation(count),[count]);
-    //useMemo also accepts second parameter i.e its dependencies and this means it will only run when the
+    //useMemo also accepts second parameter i.e its dependencies and this means it will only runs when the
     //dependencie changes so here expensiceCalculation function will only run/render when count changes and 
     //not when todo's change, without using useMemo it would have run on both
 
