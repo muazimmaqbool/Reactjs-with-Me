9490c5a94a5c36f21bfd0c135bb580719f351178
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-08-14 22:53:08 +0530

    implementation of HOC

--- src/Components/HOC.jsx
+++ src/Components/HOC.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 //Read about Higher Order Component below
 const HOC = () => {
   return (
-    <div>HOC</div>
+    <div>HOC
+      
+    </div>
   )
 }
 
@@ -19,4 +21,9 @@ export default HOC
     Add common features: authentication, logging, loading states, permissions, styling.
     Keep components pure and focused on UI, while HOC handles the extra logic.
 
+->Basic Example: With Loading Spinner
+    We want to add a loading spinner to any component that fetches data.
+    Step 1 — A simple component: UserList component
+    Step 2 - Create the HOC: withLoading
+    step 3 - use it : const UserListWithLoading = withLoading(UserList);
 */
