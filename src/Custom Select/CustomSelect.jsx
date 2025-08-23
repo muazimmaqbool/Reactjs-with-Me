d61e1b247bdf6b4700d77b48e41f316a4cc64aa8
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-08-23 17:06:55 +0530

    Basic setup

--- src/Custom Select/CustomSelect.jsx
+++ src/Custom Select/CustomSelect.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 //called from Home_Select.jsx
+/*
+->This will be called like this:
+  <Select // or CustomSelect
+    options={["React", "Vue", "Angular"]}
+    value={selectedFramework}
+    onChange={setSelectedFramework}
+  />
+*/
 const CustomSelect = () => {
   return (
     <div>CustomSelect</div>
--- src/Custom Select/Home_Select.jsx
+++ src/Custom Select/Home_Select.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+/*
+->Creating Custom Select component:
+    1)Custom Select component is created in CustomSelect.jsx file
+    2)Home_Select.jsx file is created to use CustomSelect component.
+    3)Home_Select component is imported and used in App.jsx file
 
+*/
+const programmingLanguages=["JavaScript", "Python", "Java", "C++", "Ruby", "Go", "Swift", "Kotlin", "PHP", "TypeScript"];
 const Home_Select = () => {
+  const [language, setlanguage] = useState("");
   return (
-    <div>Home_Select</div>
+    <div>
+        <h1>Custom Select Component</h1>
+        <h3>Please select your programming language:</h3>
+        {/* 
+        -> this is predefined select tag of html, we will create something similar from scratch in CustomSelect.jsx file
+        <select value={language} onChange={(e)=>setlanguage(e.target.value)}>
+            <option value="">Select Language</option>
+            {
+            programmingLanguages.map((lang, i)=>(
+                <option key={i} value={lang}>{lang}</option>
+            ))
+            }
+        </select> */}
+
+        {language!=="" && <h3>Selected Language is: {language}</h3>}
+    </div>
   )
 }
 
