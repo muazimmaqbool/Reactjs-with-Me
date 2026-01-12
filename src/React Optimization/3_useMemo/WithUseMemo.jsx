a0553c05506554c59c71c8150fdcad9395a2fbdb
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-12 22:05:53 +0530

    optimization with useMemo hook

--- src/React Optimization/3_useMemo/WithUseMemo.jsx
+++ src/React Optimization/3_useMemo/WithUseMemo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from "react";
 
 //useMemo hook is already studied in 'Hooks' folder > UseMemo.jsx
 /*
@@ -8,10 +8,52 @@ React useMemo Hook:
     The useMemo Hook only runs when one of its dependencies are updated.
     This can improve performance.
 */
-const WithUseMemo = () => {
-  return (
-    <div>WithUseMemo</div>
-  )
+
+function slowCalculation(num) {
+  console.log("Running slow calculation...");
+  let result = 0;
+  for (let i = 0; i < 100_0000_000; i++) {
+    result += num;
+  }
+  return result;
 }
 
-export default WithUseMemo
+const WithUseMemo = () => {
+  console.log("component rendered...")
+  const [number, setnumber] = useState(1);
+  const [count, setcount] = useState(0);
+
+  //➡️Without useMemo:
+  //const result = slowCalculation(number);
+
+  /*
+  Problem:
+    Clicking Increase Count
+    number does NOT change
+    BUT slowCalculation() still runs 😡
+    UI freezes
+  */
+
+  //➡️With useMemo:
+  const result = useMemo(() => {
+    return slowCalculation(number);
+  },[number]);
+  //now gets called only when number changes not when count changes
+
+  return (
+    <div>
+      <h2>Optimization with useMemo hook:</h2>
+
+      <p>Result:{result}</p>
+
+      <button onClick={() => setnumber(number + 1)}>Change Number</button>
+
+      <button onClick={() => setcount(count + 1)}>
+        Increase Count ({count})
+      </button>
+      {/* when useMemo is not used and you change the count the number remain the but still slowCalculation function gets called */}
+    </div>
+  );
+};
+
+export default WithUseMemo;
