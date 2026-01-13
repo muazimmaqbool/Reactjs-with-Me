f79c76017af08f6cad389560b83adde7aeee8d98
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-13 22:16:37 +0530

    lazy file code added

--- src/React Optimization/4_ReactLazy/LazyFile.jsx
+++ src/React Optimization/4_ReactLazy/LazyFile.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { lazy } from 'react'
 
+//Here we lazy-load the heavy component.
+//This file itself stays lightweight. The heavy code is NOT loaded initially.
+
+//loading HeavyFile via lazy keyword
+const HeavyComponent=lazy(()=>import("./HeavyFile"))
 const LazyFile = () => {
-  return (
-    <div>LazyFile</div>
-  )
+  return <HeavyComponent/>
 }
 
 export default LazyFile
