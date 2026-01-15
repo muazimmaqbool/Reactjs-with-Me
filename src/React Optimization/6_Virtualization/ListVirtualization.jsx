db0e3a74438a671a0d5c00193ec933a3e3987ac5
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-15 22:00:26 +0530

    normal list

---
+++ src/React Optimization/6_Virtualization/ListVirtualization.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+//Its not used frequently only used when list to display is large and heavy
+/*
+List Virtualization:
+    List virtualization, also known as windowing, is an optimization technique in React where you only render the items that are currently visible within the user's viewport, 
+    plus a small buffer, instead of the entire list.
+    This significantly improves performance and memory usage when dealing with large lists or tables (hundreds or thousands of items) by drastically reducing the number of DOM nodes. 
+
+->install this dependency: npm install react-window
+
+->We will create a file called 'tempData.js' which contains dummpy data that we are going to display first via 'Normal List' and then via 'Virtualization'
+*/
+import { products } from './tempData'; //contains temp data
+
+const Item = ({ item }) => {
+  return (
+    <div style={styles.item}>
+      <p><strong>{item.productName}</strong></p>
+      <p>{item.isAvailable}</p>
+      <p>{item.price}</p>
+    </div>
+  );
+};
+
+ function NormalList() {
+  console.log("Normal List Rendered");
+
+  return (
+    <div style={styles.container}>
+      
+      {products.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
+const ListVirtualization = () => {
+  return (
+    <div>
+      <h2>Normal List (1000 items)</h2>
+      <NormalList/>
+      <h2>Virtualized list</h2>
+    </div>
+  )
+}
+/*
+->Problem with normal list:
+    Renders 1000 DOM nodes
+    Slow scrolling
+    Re-renders are expensive
+    UI lag on low-end devices
+*/
+
+
+const styles = {
+  container: {
+    height: "400px",
+    overflowY: "auto",
+    border: "1px solid #ccc",
+    padding: "8px",
+  },
+  item: {
+    padding: "10px",
+    marginBottom: "8px",
+    border: "1px solid #ddd",
+    borderRadius: "4px",
+  },
+};
+export default ListVirtualization
